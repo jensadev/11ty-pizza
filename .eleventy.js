61fb7e6642f318b1180ed606138c54ad5b48a3c9
Jens Andreasson <jens.andreasson@ga.ntig.se>
2021-10-05 12:05:20 +0200

    ✨ sass

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,4 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("./src/css");
-    eleventyConfig.addWatchTarget("./src/css");
-
     return {
         dir: {
             input: 'src',
